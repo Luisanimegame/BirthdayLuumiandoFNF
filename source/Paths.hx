14e26f0ddb9053354fc86a9343c2c1f843a17233
Bonny Wuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2024-10-21 04:05:51 -0300

    Sendo burro como sempre

--- source/Paths.hx
+++ source/Paths.hx
@@ -218,14 +218,14 @@ class Paths
 		return file;
 	}
 
-	inline static public function vocals(song:String):Any
+	inline static public function voices(song:String):Any
 	{
 		#if html5
 		return 'songs:assets/songs/${formatToSongPath(song)}/Voices-player.$SOUND_EXT';
 		#else
 		var songKey:String = '${formatToSongPath(song)}/Voices-player';
-		var vocals = returnSound('songs', songKey);
-		return vocals;
+		var voices = returnSound('songs', songKey);
+		return voices;
 		#end
 	}
 	
