90cfc4b1e6739ff7525d007e6907c7379d822632
Bonny Wuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2024-11-01 01:52:21 -0300

    Update

--- assets/preload/data/luumiando/cutcene.lua
+++ assets/preload/data/luumiando/cutcene.lua
@@ -2,8 +2,10 @@ local allowCountdown = false
 function onStartCountdown()
 if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
 startVideo('meowando');
+setProperty('inCutscene', true)
 allowCountdown = true;
 return Function_Stop;
 end
 return Function_Continue;
+setProperty('inCutscene', false)
 end
